e32b33f3641a696318d544915c35d6e3025f80dd
ProxyNeko <proxyneko@users.noreply.github.com>
2019-11-02 00:57:29 +0000

    Code cleanup.

--- src/main/java/com/mcmoddev/naturallyspawningvex/Config.java
+++ src/main/java/com/mcmoddev/naturallyspawningvex/Config.java
@@ -6,34 +6,68 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
 
-public class Config {
+class Config {
 
-	public static final ServerConfig SERVER;
-	public static final ForgeConfigSpec SERVER_SPECIFICATION;
+	/**
+	 *
+	 */
+	static final ServerConfig SERVER;
+	/**
+	 *
+	 */
+	static final ForgeConfigSpec SERVER_SPECIFICATION;
+	/**
+	 * The default spawn weight of Vex.
+	 */
+	private static final int VEX_DEFAULT_SPAWN_WEIGHT = 35;
 
 	static {
-		Pair<ServerConfig, ForgeConfigSpec> specificationPair = new ForgeConfigSpec.Builder().configure(ServerConfig::new);
+		Pair<ServerConfig, ForgeConfigSpec> specificationPair =
+			new ForgeConfigSpec.Builder().configure(ServerConfig::new);
 		SERVER_SPECIFICATION = specificationPair.getRight();
 		SERVER = specificationPair.getLeft();
 	}
 
+	/**
+	 *
+	 */
 	public static class ServerConfig {
-		public final ForgeConfigSpec.BooleanValue VexSpawnNaturally;
-		public final ForgeConfigSpec.IntValue VexSpawnWeight;
-		public final ForgeConfigSpec.ConfigValue<List<? extends String>> BiomeWhitelist;
-		public final ForgeConfigSpec.ConfigValue<List<? extends String>> BiomeBlacklist;
+		/**
+		 * Boolean value to see if Vex should spawn naturally in all biomes.
+		 */
+		final ForgeConfigSpec.BooleanValue vexSpawnNaturally;
 
-		ServerConfig(ForgeConfigSpec.Builder builder) {
-			VexSpawnNaturally = builder.comment("If Vex should spawn naturally in the world.")
+		/**
+		 * Set the spawn weight of Vex.
+		 */
+		final ForgeConfigSpec.IntValue vexSpawnWeight;
+
+		/**
+		 * The biome whitelist. Only biomes listed here will spawn Vex.
+		 */
+		final ForgeConfigSpec.ConfigValue<List<? extends String>> biomeWhitelist;
+
+		/**
+		 * The biome blacklist. Biomes listed here will not have Vex spawn in them.
+		 */
+		final ForgeConfigSpec.ConfigValue<List<? extends String>> biomeBlacklist;
+
+		/**
+		 * @param builder The config builder itself.
+		 */
+		ServerConfig(final ForgeConfigSpec.Builder builder) {
+			vexSpawnNaturally = builder.comment("If Vex should spawn naturally in the world.")
 				.define("enableNaturalSpawning", true);
 
-			VexSpawnWeight = builder.comment("If -1, the default spawn weight will be used. (Default = 35)")
-				.defineInRange("spawnWeight", 35, -1, Integer.MAX_VALUE);
+			vexSpawnWeight = builder.comment("If -1, the default spawn weight will be used. (Default = 35)")
+				.defineInRange("spawnWeight", VEX_DEFAULT_SPAWN_WEIGHT, -1, Integer.MAX_VALUE);
 
-			BiomeWhitelist = builder.comment("If biomes are specified here, Vex will ONLY spawn in these biomes. (The blacklist is ignored while this is set!)")
+			biomeWhitelist = builder.comment("If biomes are specified here, Vex will ONLY spawn in these biomes."
+				+ " (The blacklist is ignored while this is set!)")
 				.defineList("whitelist", Lists.newArrayList(), o -> o instanceof String);
 
-			BiomeBlacklist = builder.comment("If the whitelist is not used, use this list to specify the biomes that Vex should not spawn in.")
+			biomeBlacklist = builder.comment("If the whitelist is not used, use this list to specify the biomes that "
+				+ "Vex should not spawn in.")
 				.defineList("blacklist", Lists.newArrayList("minecraft:void"), o -> o instanceof String);
 		}
 	}
--- src/main/java/com/mcmoddev/naturallyspawningvex/NaturallySpawningVex.java
+++ src/main/java/com/mcmoddev/naturallyspawningvex/NaturallySpawningVex.java
@@ -1,10 +1,8 @@
 package com.mcmoddev.naturallyspawningvex;
 
 import com.google.common.collect.Sets;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.monster.VexEntity;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -28,12 +26,21 @@ public class NaturallySpawningVex {
 	/**
 	 * Set the mods ID.
 	 */
-	public static final String MODID = "naturallyspawningvex";
+	static final String MODID = "naturallyspawningvex";
 
 	/**
 	 * Setup the logger for the mod.
 	 */
 	private static final Logger LOGGER = LogManager.getLogger("Naturally Spawning Vex");
+
+	/**
+	 * The minimum amount of Vex that will spawn in a group.
+	 */
+	private static final int VEX_MIN_SPAWN_GROUP = 1;
+	/**
+	 * The maximum amount of Vex that will spawn in a group.
+	 */
+	private static final int VEX_MAX_SPAWN_GROUP = 6;
 
 	/**
 	 * Register things.
@@ -49,43 +56,58 @@ public class NaturallySpawningVex {
 	 *
 	 * @param event the event fired.
 	 */
-	private void fingerprintViolation(FMLFingerprintViolationEvent event) {
-		LOGGER.error("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been " +
-			"tampered with. This version will NOT be supported! Please download the mod from CurseForge for a " +
-			"supported and signed version of the mod.");
+	private void fingerprintViolation(final FMLFingerprintViolationEvent event) {
+		LOGGER.error("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been "
+			+ "tampered with. This version will NOT be supported! Please download the mod from CurseForge for a "
+			+ "supported and signed version of the mod.");
 	}
 
+	/**
+	 * A collection of all the biomes in the game currently.
+	 */
 	private static Collection<Biome> biomes = null;
+
+	/**
+	 *
+	 */
 	private static Biome.SpawnListEntry entry = null;
 
+	/**
+	 * @param event The config load event.
+	 */
 	@SubscribeEvent
-	public static void onLoad(ModConfig.Loading event) {
+	public static void onLoad(final ModConfig.Loading event) {
 		if (event.getConfig().getSpec() != Config.SERVER_SPECIFICATION) {
 			return;
 		}
 
 		if (entry != null) {
-			biomes.stream().map(biome -> biome.getSpawns(EntityClassification.MONSTER)).forEach(list -> list.remove(entry));
+			biomes.stream().map(biome -> biome.getSpawns(EntityClassification.MONSTER)).forEach(list ->
+				list.remove(entry));
 			biomes = Collections.emptyList();
 		}
 
-		if (Config.SERVER.VexSpawnNaturally.get()) {
-			int currentWeight = Config.SERVER.VexSpawnWeight.get();
+		if (Config.SERVER.vexSpawnNaturally.get()) {
+			int currentWeight = Config.SERVER.vexSpawnWeight.get();
 
 			if (currentWeight > 0) {
 				biomes = ForgeRegistries.BIOMES.getValues();
-				if (Config.SERVER.BiomeWhitelist.get() != null && Config.SERVER.BiomeWhitelist.get().size() > 0) {
-					Set<String> whitelist = Sets.newHashSet(Config.SERVER.BiomeWhitelist.get());
-					biomes = biomes.stream().filter(b -> whitelist.contains(b.getRegistryName().toString())).collect(Collectors.toList());
+				if (Config.SERVER.biomeWhitelist.get() != null && Config.SERVER.biomeWhitelist.get().size() > 0) {
+					Set<String> whitelist = Sets.newHashSet(Config.SERVER.biomeWhitelist.get());
+					biomes = biomes.stream().filter(b ->
+						whitelist.contains(b.getRegistryName().toString())).collect(Collectors.toList());
 				} else {
-					if (Config.SERVER.BiomeBlacklist.get() != null && Config.SERVER.BiomeBlacklist.get().size() > 0) {
-						Set<String> blacklist = Sets.newHashSet(Config.SERVER.BiomeBlacklist.get());
-						biomes = biomes.stream().filter(b -> !blacklist.contains(b.getRegistryName().toString())).collect(Collectors.toList());
+					if (Config.SERVER.biomeBlacklist.get() != null && Config.SERVER.biomeBlacklist.get().size() > 0) {
+						Set<String> blacklist = Sets.newHashSet(Config.SERVER.biomeBlacklist.get());
+						biomes = biomes.stream().filter(b ->
+							!blacklist.contains(b.getRegistryName().toString())).collect(Collectors.toList());
 					}
 				}
 
-				entry = new Biome.SpawnListEntry(EntityType.VEX, Config.SERVER.VexSpawnWeight.get(), 1, 5);
-				biomes.stream().map(biome -> biome.getSpawns(EntityClassification.MONSTER)).forEach(list -> list.add(entry));
+				entry = new Biome.SpawnListEntry(EntityType.VEX, Config.SERVER.vexSpawnWeight.get(),
+					VEX_MIN_SPAWN_GROUP, VEX_MAX_SPAWN_GROUP);
+				biomes.stream().map(biome -> biome.getSpawns(EntityClassification.MONSTER)).forEach(list ->
+					list.add(entry));
 			}
 		}
 	}
